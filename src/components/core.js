9bf8d531b156c8a50a7b173a790b169042763811
Nikhil Gupta <guptan@Nikhils-MacBook-Air.local>
2021-04-24 20:42:13 +0545

    Changes

--- src/components/core.js
+++ src/components/core.js
@@ -1,14 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import TimelineIcon from "@material-ui/icons/Timeline";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
@@ -23,24 +19,42 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.primary.main,
+  },
+  success: {
+    color: theme.palette.success.main,
   },
   form: {
     width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    margin: theme.spacing(3, 0, 5),
   },
 }));
 
-function handleSubmit(e) {
-  e.preventDefault();
-  e.target.reset();
-}
-
 export default function Core() {
-  
+  const [entry, setEntry] = useState("");
+  const [stoploss, setStoploss] = useState("");
+  const [risk, setRisk] = useState("");
+
+  const dif = entry - stoploss;
+  const diff = dif > 0 ? dif : "Entry should be greater than Stoploss";
+  const quantity =
+    diff > 0 && risk === ""
+      ? "Enter Risk Amount"
+      : diff > 0 && risk > 0
+      ? Math.round(risk / diff)
+      : 0;
+  const rupeeTarget = diff > 0 ? diff * 2 : 0;
+  const target = diff > 0 ? parseInt(entry) + parseInt(rupeeTarget) : 0;
+
+  function resetInput() {
+    setEntry("");
+    setStoploss("");
+    setRisk("");
+  }
+
   const classes = useStyles();
 
   return (
@@ -48,17 +62,10 @@ export default function Core() {
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
+          <TimelineIcon />
         </Avatar>
-        <Typography component="h1" variant="h5">
-          Swing Trading Risk Management
-        </Typography>
-        <form
-          className={classes.form}
-          noValidate
-          autoComplete="off"
-          onSubmit={handleSubmit.bind(this)}
-        >
+        <Typography variant="h4">Swing Trading Risk Management</Typography>
+        <div className={classes.form}>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -67,9 +74,12 @@ export default function Core() {
                 // required
                 fullWidth
                 id="entry"
+                type="number"
                 label="Entry"
                 color="success"
-                autoFocus
+                value={entry}
+                onChange={(event) => setEntry(event.target.value)}
+                // autoFocus
               />
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -79,13 +89,19 @@ export default function Core() {
                 fullWidth
                 id="stopLoss"
                 label="Stop Loss"
+                type="number"
                 name="stopLoss"
+                value={stoploss}
+                onChange={(event) => setStoploss(event.target.value)}
                 color="secondary"
               />
             </Grid>
             <Grid item xs={12}>
-              <Typography color="Secondary">
-                Difference of Entry and Stop Loss: {}
+              <Typography variant="subtitle1">
+                Difference of Entry and Stop Loss:
+              </Typography>
+              <Typography color="secondary" variant="subtitle2">
+                {diff}
               </Typography>
             </Grid>
             <Grid item xs={12}>
@@ -95,33 +111,42 @@ export default function Core() {
                 fullWidth
                 id="risk"
                 label="How much Risk?"
+                type="number"
                 name="risk"
                 color="secondary"
+                value={risk}
+                onChange={(event) => setRisk(event.target.value)}
               />
             </Grid>
             <Grid item xs={12}>
-              <Typography color="Primary">
-                Buy Quantity: {}
-                <br />
-                Rupee Target:{}
+              <Typography variant="subtitle1">Quantity to buy:</Typography>
+              <Typography color="primary" variant="subtitle2">
+                {quantity}
               </Typography>
             </Grid>
             <Grid item xs={12}>
-            <Typography color="success">
-                Target: {}
+              <Typography variant="subtitle1">Rupee Target:</Typography>
+              <Typography color="primary" variant="subtitle2">
+                {rupeeTarget}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="subtitle1">Target:</Typography>
+              <Typography className={classes.success} variant="subtitle2">
+                {target}
               </Typography>
             </Grid>
           </Grid>
           <Button
-            type="submit"
             fullWidth
             variant="contained"
-            color="primary"
+            color="secondary"
             className={classes.submit}
+            onClick={resetInput}
           >
             Reset
           </Button>
-        </form>
+        </div>
       </div>
     </Container>
   );
